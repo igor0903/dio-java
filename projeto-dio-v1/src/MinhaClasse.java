6f9e7ef93cb5d365bbee43a138a4e27511effbd0
igor melo <igor0903@live.com>
2024-05-05 00:27:30 +0100

    classes e metodos

--- projeto-dio-v1/src/MinhaClasse.java
+++ projeto-dio-v1/src/MinhaClasse.java
@@ -2,7 +2,22 @@ public class MinhaClasse {
     
     public static void main(String[] args) { // metodo da classe MINHA
         final String BR = "Brasil"; 
-        System.out.println(BR);
+        
+        String meuNome = "Igor";
+        int anoFabricacao = 1930;
+        boolean verdade = true;
+
+        String primeiroNome = "Jonas ";
+        String sobrenome = "Esticado";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, sobrenome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String sobrenome){
+        return primeiroNome.concat(sobrenome);
     }
 
 }
